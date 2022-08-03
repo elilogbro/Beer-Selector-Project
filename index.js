702bc896dbaf01f8a7e44ac9c2ef815758bcb6d9
Wendy You <wyou130@gmail.com>
2022-08-03 10:10:11 -0400

    new CSS, updated rating HTML, added form click event and handle form function

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const ul = document.getElementById('beer-name')
 const dropdown = document.getElementById('dropdown')
 
 let beerList = [];
-const lowCheckbox = document.getElementById('low-abv')
-const highCheckbox = document.getElementById('high-abv')
-const extraHighCheckbox = document.getElementById('extra-high-abv')
 const detailImage = document.querySelector('#detail-image')
 const detailName = document.querySelector('#detail-name')
 const detailTagline = document.querySelector('#detail-tagline')
 const detailDescription = document.querySelector('#detail-description')
 const detailAbv = document.querySelector('#detail-abv')
 const detailIbu = document.querySelector('#detail-ibu')
+const ratingForm = document.querySelector('#rating-form')
+const detailRating = document.querySelector('#detail-rating')
 
-// let currentBeer = [];
 let currentBeer = {}
+let count = 0
 
 //fetch
 fetch(url)
     .then(res => res.json())
     .then(beers => {
         renderList(beers)
-        beerList = beers
-        // currentBeer = beers
+        // beerList = beers
+        handleForm()
     })
 
 // render list     
@@ -32,15 +31,6 @@ function renderList(beers) {
     beers.forEach(beer => {
         const li = document.createElement('li')
         const beerName = beer.name
-        if (beer.abv <= 6) {
-            li.className = "low-ABV"
-        }
-        else if (beer.abv > 6 && beer.abv <= 10) {
-            li.className = "high-ABV"
-        }
-        else {
-            li.className = "extra-high-ABV"
-        }
         li.textContent = beerName
         ul.append(li)
         li.addEventListener('click', () => renderBeerDetails(beer))
@@ -53,27 +43,43 @@ function abvRange(beerList) {
     let filteredBeers
     switch(abv) {
         case '4-6% ABV' :
-            filteredBeers = beerList.filter(beer => beer.abv >= 4 && beer.abv <= 6);
+            filteredBeers = beers.filter(beer => beer.abv >= 4 && beer.abv <= 6);
             break;
         case '6-10% ABV' :
-            filteredBeers = beerList.filter(beer => beer.abv > 6 && beer.abv <=10);
+            filteredBeers = beers.filter(beer => beer.abv > 6 && beer.abv <=10);
             break;
         case '10+% ABV' :
-            filteredBeers = beerList.filter(beer => beer.abv > 10);
+            filteredBeers = beers.filter(beer => beer.abv > 10);
             break;
     }
     ul.textContent = ''
     renderList(filteredBeers)
 }
 
+//render beer details when clicked
 function renderBeerDetails(beer) {
-    currentBeer = beer 
+    currentBeer = beer
+    count = 0
     detailName.textContent = beer.name
     detailImage.src = beer.image_url
     detailTagline.textContent = beer.tagline
     detailDescription.textContent = beer.description
     detailAbv.textContent = `ABV: ${beer.abv}`
     detailIbu.textContent = `IBU: ${beer.ibu}`
+    currentBeer.rating = ""
+    detailRating.textContent = `Rating: ${currentBeer.rating}`
+}
+
+//form functions
+function handleForm() {
+    ratingForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        count++
+        currentBeer.rating = +e.target[id="rating"].value + +currentBeer.rating
+        let averageRating = (currentBeer.rating) / count
+        detailRating.textContent = `Rating: ${averageRating}`
+        ratingForm.reset() 
+    })
 }
 
 //event listeners
